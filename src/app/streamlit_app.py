dd6623e9d28648c989b2637839f69d7277e5fe1b
spuerta10 <puertasantiago2002@gmail.com>
2025-04-10 13:26:22 -0500

    feat: Added batch load and prediction support

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -1,13 +1,33 @@
+from io import BytesIO
+from time import sleep
 from typing import Any, ClassVar
 
-from streamlit import divider, error, form, form_submit_button, success, title
+from pandas import DataFrame
+from streamlit import (
+    button,
+    divider,
+    error,
+    form,
+    form_submit_button,
+    spinner,
+    success,
+    tabs,
+    title,
+)
 
+from src.controller.base_batch_controller import BaseBatchController
 from src.controller.base_controller import BaseController
+from src.controller.streamlit_controller import StreamlitController
 from src.view import (
     BaseView,
     CreditRequestView,
+    DonwloadPredictionsView,
+    FilePreviewView,
     FinancialStatusView,
     PersonalInformationView,
+    PredictionsPreviewView,
+    PredictionsStatsView,
+    UploadFileView,
 )
 
 
@@ -25,19 +45,35 @@ class StreamlitApp:
     \nLet's work on it together.
     """
 
-    def __init__(self, controller: BaseController) -> None:
-        self.__views: list[BaseView] = [
-            CreditRequestView(),
-            FinancialStatusView(),
-            PersonalInformationView(),
-        ]
-        self.__controller: BaseController = controller
+    def __init__(self, controller: StreamlitController) -> None:
+        assert isinstance(controller, BaseController) and isinstance(
+            controller, BaseBatchController
+        )  # controller must have batch and base (form) capabilities
+
+        self.__views: dict[str, list[BaseView]] = {
+            "form": [
+                CreditRequestView(),
+                FinancialStatusView(),
+                PersonalInformationView(),
+            ],
+            "batch": [
+                UploadFileView(),
+                FilePreviewView(),
+                PredictionsPreviewView(),
+                PredictionsStatsView(),
+                DonwloadPredictionsView(),
+            ],
+        }
+        self.__controller: StreamlitController = controller
 
     def run(self) -> None:
         title("Your Credit Request")
-        with form("prediction_form"):
+
+        tab1, tab2 = tabs(["Credit Application", "Bulk Credit Applications"])
+
+        with tab1, form("prediction_form"):
             form_data: dict[str, Any] = {}
-            for view in self.__views:
+            for view in self.__views["form"]:
                 form_data.update(view.render())
                 divider()
 
@@ -48,3 +84,29 @@ class StreamlitApp:
                     success(self.CREDIT_APPROVED_MESSAGE)
                 else:
                     error(self.CREDIT_NOT_APPROVED_MESSAGE)
+
+        with tab2:
+            upload_view, preview_view, predictions_view, stats_view, download_view = self.__views[
+                "batch"
+            ]
+            uploaded_file: BytesIO = upload_view.render()
+            if uploaded_file:
+                try:
+                    preview_df: DataFrame = self.__controller.get_file_preview(uploaded_file)
+                except Exception as e:
+                    error(e)
+                preview_view.render(preview_df)
+                if button("Predict Credit Approval"):
+                    with spinner("Processing data and making predictions..."):
+                        try:
+                            predictions: DataFrame = self.__controller.handle_batch_prediction(
+                                uploaded_file
+                            )
+                        except Exception as e:
+                            error(e)
+                        else:
+                            sleep(3)
+                            predictions_view.render(predictions.sample(7, ignore_index=True))
+                            approval_rate: float = self.__controller.get_approval_rate(predictions)
+                            stats_view.render(approval_rate, 100 - approval_rate)
+                            download_view.render(predictions.to_csv(index=False))
