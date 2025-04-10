48663996f32c58d5b0dd75c939c6b7492e192f8f
spuerta10 <puertasantiago2002@gmail.com>
2025-04-09 20:39:15 -0500

    chore: Added init.py for module init

---
+++ src/view/__init__.py
@@ -0,0 +1,11 @@
+from src.view.base_view import BaseView
+from src.view.credit_request_view import CreditRequestView
+from src.view.financial_status_view import FinancialStatusView
+from src.view.personal_information_view import PersonalInformationView
+
+__all__ = [
+    "BaseView",
+    "CreditRequestView",
+    "FinancialStatusView",
+    "PersonalInformationView",
+]
