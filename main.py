7ab4df44de112f3ff224a1335cc592f0781e5135
spuerta10 <puertasantiago2002@gmail.com>
2025-04-09 20:43:04 -0500

    feat: Added application execution entrypoint

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.app.streamlit_app import StreamlitApp
+from src.controller.streamlit_controller import StreamlitController
+from src.service.prediction_service import PredictionService
+from src.service.transformations import (
+    clean_features,  # noqa
+    get_features_names,  # noqa
+    remove_outliers,  # noqa
+)
+
+if __name__ == "__main__":
+    prediction_service: PredictionService = PredictionService(
+        r"models/credit_classification-logistic_regression-v1.joblib"
+    )
+    streamlit_controller: StreamlitController = StreamlitController(service=prediction_service)
+    app: StreamlitApp = StreamlitApp(controller=streamlit_controller)
+    app.run()
